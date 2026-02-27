b4025737e97cd043572809aabbc51ec646da4d99
studyarisina-max <studyarisina@gmail.com>
2026-02-27 22:43:34 +0530

    Update api.ts

--- api.ts
+++ api.ts
@@ -2,11 +2,11 @@ import axios from 'axios';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
 const API = axios.create({
-  // Using your specific IPv4 address
+ 
   baseURL: 'http://192.168.29.136:5000/api',
 });
 
-// This automatically adds the token to every request after you login
+
 API.interceptors.request.use(async (config) => {
   const token = await AsyncStorage.getItem('token');
   if (token) {
